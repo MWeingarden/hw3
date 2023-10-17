6e3a8d63e00b8362b73fe15dafab7c66682da1f5
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-17 17:38:14 -0500

    Create view-teams.php

---
+++ view-teams.php
@@ -0,0 +1,14 @@
+<h1>NFL Teams</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Team Name</th>
+    </thead>
+    <tbody>
+<?php
+while ($team = $teams)
+?>
+    </tbody>
+  </table>
+</div>
