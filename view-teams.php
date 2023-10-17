d5b00004dc13498366020ee5af4a2722458af9dc
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-17 17:43:11 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -2,12 +2,21 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
+      <tr>
       <th>ID</th>
       <th>Team Name</th>
+      </tr>
     </thead>
     <tbody>
 <?php
-while ($team = $teams)
+while ($team = $teams->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $team['team_id']; ?></td>
+    <td><?php echo $team['team_name']; ?></td>
+  </tr>
+<?php
+}
 ?>
     </tbody>
   </table>
