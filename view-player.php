5acbbea7bda9c0b0472e24a1a0c40737436fbc89
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 13:35:55 -0500

    Update view-player.php

--- view-player.php
+++ view-player.php
@@ -1,1 +1,25 @@
-
+<h1>NFL Players</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Player Name</th>
+      <th>Position</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($player = $players->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $player['player_id']; ?></td>
+    <td><?php echo $player['player_name']; ?></td>
+    <td><?php echo $player['position']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
