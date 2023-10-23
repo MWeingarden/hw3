3e2a0c51ec458df590ad1058aa959e9f89cc9d67
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 13:29:17 -0500

    Update players.php

--- players.php
+++ players.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-players.php");
 
+$pageTitle = "Players";
+include "view-header.php";
+$teams = selectTeams();
+include "view-players.php";
+include "view-footer.php";
+?>
