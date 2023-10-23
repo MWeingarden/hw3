c33f37bd09028af9ba68f785768ab58a3ae33f47
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 16:37:45 -0500

    Update teams-with-coaches.php

--- teams-with-coaches.php
+++ teams-with-coaches.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams-with-coaches.php");
 
+$pageTitle = "Teams With Coaches";
+include "view-header.php";
+$teams = selectTeams();
+include "view-teams-with-coaches.php";
+include "view-footer.php";
+?>
