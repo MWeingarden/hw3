4e4e063adad3e34d70b3176920b2e423672174db
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 17:32:35 -0500

    Update model-coach.php

--- model-coach.php
+++ model-coach.php
@@ -1,1 +1,16 @@
-
+<?php
+function selectTeamsByPlayer($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT coach_id, coach_name FROM Coach c JOIN Team t ON t.team_id = c.team_id WHERE t.team_id =?");
+        $stmt->bind_param("i", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
