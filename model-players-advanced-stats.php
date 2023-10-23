4fbf7ca3f10da2d6faba0a081c2dbd57fc32d61b
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 16:21:21 -0500

    Update and rename model-players-team.php to model-players-advanced-stats.php

--- model-players-advanced-stats.php
+++ model-players-advanced-stats.php
@@ -1,8 +1,8 @@
 <?php
-function selectPlayersTeam($pid) {
+function selectPlayersAdvancedStats($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id, player_name, position, team_name FROM `Player` p JOIN Team t on t.team_id = p.team_id WHERE player_id =?");
+        $stmt = $conn->prepare("SELECT player_name, receptions, reception_targets, reception_yards, rushing_attempts, rushing_yards, rushing_yards_per_attempt FROM Player WHERE player_id=?");
         $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
