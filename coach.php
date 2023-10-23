88696f700a92d1f19fc664afbae51281d1a3dd90
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 17:29:14 -0500

    Update coach.php

--- coach.php
+++ coach.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-coach.php");
 
+$pageTitle = "Head Coaches";
+include "view-header.php";
+$team = selectCoach();
+include "view-coach.php";
+include "view-footer.php";
+?>
