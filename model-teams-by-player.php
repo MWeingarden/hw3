6091680eaf3ada1faae9525bdb8c682f59a81f6e
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 15:17:38 -0500

    Create model-teams-by-player.php

---
+++ model-teams-by-player.php
@@ -0,0 +1,16 @@
+<?php
+function selectTeamsByPlayer($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name, position, FROM `Player` p JOIN Team t on t.team_id = p.team_id WHERE player_id =?");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
