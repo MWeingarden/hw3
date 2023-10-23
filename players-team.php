87c2a448fb79f0124caa048ec2eb219da6f40245
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 14:34:19 -0500

    Update and rename players-statistics.php to players-team.php

---
+++ players-team.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-players-team.php");
+
+$pageTitle = "Player Team";
+include "view-header.php";
+$players = selectPlayersTeam($_GET['player_id');
+include "view-players-team.php";
+include "view-footer.php";
+?>
