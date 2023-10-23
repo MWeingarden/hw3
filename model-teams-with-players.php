9dbdb16398eb38018c49d1fdfc065968c2164eb7
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 16:51:50 -0500

    Update model-teams-with-players.php

--- model-teams-with-players.php
+++ model-teams-with-players.php
@@ -12,4 +12,19 @@ function selectTeams() {
         throw $e;
     }
 }
+
+function selectTeamsByPlayer($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name, position FROM Player p JOIN Team t on t.team_id = p.team_id WHERE p.team_id =?");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
