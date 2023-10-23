138b3a5897a994a0d0195becb77124a87d6e4de3
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 17:16:42 -0500

    Update view-teams-with-players.php

--- view-teams-with-players.php
+++ view-teams-with-players.php
@@ -7,13 +7,16 @@ while ($team = $teams->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $team['team_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $players = selectPlayersByTeam($team['team_id']);
   while($player = $players->fetch_assoc()) {
 ?>
+    <li class="list-group-item"><?php echo $team['player_name']; ?> - <?php echo $team['position']; ?></li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Sponsored by the NFL</small></p>
     </div>
