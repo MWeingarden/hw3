6d832f7765bc5d96ce4d26e5972fa1f647cd4a6a
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 16:24:50 -0500

    Update and rename view-players-team.php to view-players-advanced-stats.php

---
+++ view-players-advanced-stats.php
@@ -0,0 +1,33 @@
+<h1>Advanced Stats</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Player Name</th>
+      <th>Receptions</th>
+      <th>Reception Targets</th>
+      <th>Reception Yards</th>
+      <th>Rushing Attempts</th>
+      <th>Rushing Yards</th>
+      <th>Rushing Yards Per Attempt</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($player = $players->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $player['player_name']; ?></td>
+    <td><?php echo $player['receptions']; ?></td>
+    <td><?php echo $player['reception_targets']; ?></td>
+    <td><?php echo $player['reception_yards']; ?></td>
+    <td><?php echo $player['rushing_attempts']; ?></td>
+    <td><?php echo $player['rusing_yards']; ?></td>
+    <td><?php echo $player['rushing_yards_per_attempt']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
