eb5e47c8e7f61fb8cf878a0a9d9b3f34898bb4a7
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 15:00:35 -0500

    Update players-team.php

--- players-team.php
+++ players-team.php
@@ -4,7 +4,7 @@ require_once("model-players-team.php");
 
 $pageTitle = "Player Team";
 include "view-header.php";
-$players = selectPlayersTeam($_GET['player_id']);
+$players = selectPlayersTeam($_GET['id']);
 include "view-players-team.php";
 include "view-footer.php";
 ?>
