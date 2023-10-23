b7c026f463530ad33d485d0d67e722251590c068
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 15:03:31 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -6,6 +6,8 @@
       <th>ID</th>
       <th>Player Name</th>
       <th>Position</th>
+      <th>Receptions</th>
+      <th>Rushing Attempts</th>
       <th>Team</th>
       </tr>
     </thead>
@@ -17,6 +19,8 @@ while ($player = $players->fetch_assoc()) {
     <td><?php echo $player['player_id']; ?></td>
     <td><?php echo $player['player_name']; ?></td>
     <td><?php echo $player['position']; ?></td>
+    <td><?php echo $player['receptions']; ?></td>
+    <td><?php echo $player['rushing_attempts']; ?></td>
     <td><a href="players-team.php?id=<?php echo $player['player_id']; ?>">Team</a></td>
   </tr>
 <?php
