c4f63756870718ef1bdb7b3972f55642e3844380
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 14:24:53 -0500

    Update players-statistics.php

--- players-statistics.php
+++ players-statistics.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-players.php");
+require_once("model-players-statistics.php");
 
-$pageTitle = "Players";
+$pageTitle = "Player Statistics";
 include "view-header.php";
-$players = selectPlayers();
+$players = selectPlayersStatistics($_GET['id);
 include "view-players.php";
 include "view-footer.php";
 ?>
