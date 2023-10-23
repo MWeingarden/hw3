6e683010dd2d49e89527d8b79571e68995513cc2
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 17:33:56 -0500

    Update view-coach.php

--- view-coach.php
+++ view-coach.php
@@ -1,1 +1,20 @@
-
+<h1>NFL Coaches</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Coach Name</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($team = $teams->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $team['coach_id']; ?></td>
+    <td><?php echo $team['coach_name']; ?></td>
+  </tr>
+<?php
+}
+?>
