8b19216e4dd4826e0ed0d84c161ecb1658189799
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-23 14:48:35 -0500

    Update model-players-team.php

--- model-players-team.php
+++ model-players-team.php
@@ -1,8 +1,9 @@
 <?php
-function selectPlayersTeam() {
+function selectPlayersTeam($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id, player_name, position FROM `Player`");
+        $stmt = $conn->prepare("SELECT player_id, player_name, position, team_name FROM `Player` p JOIN Team t on t.team_id = p.team_id WHERE player_id =?");
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
