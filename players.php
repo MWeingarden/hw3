233cd6c2fc06e4214ff091f4eb764d2a2eb3f8a4
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-24 14:50:57 -0500

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,14 @@ require_once("model-players.php");
 
 $pageTitle = "Players";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertPlayer($_POST['pNumber'], $_POST
+  }
+}
+  
 $players = selectPlayers();
 include "view-players.php";
 include "view-footer.php";
