459ef2eb8e63841aa92570249c10f7d4e83fdc7a
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-24 15:40:40 -0500

    Update players.php

--- players.php
+++ players.php
@@ -9,7 +9,7 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
       if (insertPlayer($_POST['tNumber'], $_POST['pName'], $_POST['pPosition'], $_POST['pReceptions'], $_POST['pReceptionYards'], $_POST['pReceptionTargets'], $_POST['pRushingAttempts'], $_POST['pRushingYards'], $_POST['pRushingYardsPerAttempt'])) {
-        echo '<div class="alert alert-success" role="alert">Player Added!</div>"';
+        echo '<div class="alert alert-success" role="alert">Player Added!</div>';
       }
       else {
         echo '<div class="alert alert-danger" role="alert">Error!</div>';
