93611808d47c729a010cdafa6b07321e604babad
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-24 14:56:10 -0500

    Update players.php

--- players.php
+++ players.php
@@ -8,7 +8,8 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertPlayer($_POST['pNumber'], $_POST
+      insertPlayer($_POST['tID'], $_POST['pName'], $_POST['position'], $_POST['receptions'], $_POST['recepYards'], $_POST['recepTargets'], $_POST['rushAttempts'], $_POST['rushYards'], $_POST['rushYardsPerAttempt']);
+      break;
   }
 }
   
