2f1847781926bd6353df4b008335887e6854bc06
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-24 16:17:51 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -32,7 +32,7 @@ while ($player = $players->fetch_assoc()) {
     <td></td>
     <td>
       <form method="post" action="">
-        <input type="hidden" name="pid" value="<?php echo $player['player_id']; ?>">
+        <input type="hidden" name="pID" value="<?php echo $player['player_id']; ?>">
         <input type="hidden" name="actionType" value="Delete">
         <button type="submit" class="btn btn-primary">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
