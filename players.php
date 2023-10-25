6a2b65a50dd964f2d564937bf0c9cf448352a192
MWeingarden <122938141+MWeingarden@users.noreply.github.com>
2023-10-25 14:38:18 -0500

    Update players.php

--- players.php
+++ players.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])) {
       }
       break;
      case "Edit":
-      if (updatePlayer($_POST['tID'], $_POST['tName'], $_POST['pName'], $_POST['pPosition'], $_POST['pReceptions'], $_POST['pReceptionYards'], $_POST['pReceptionTargets'], $_POST['pRushingAttempts'], $_POST['pRushingYards'], $_POST['pRushingYardsPerAttempt'], $_POST['pID'])) {
+      if (updatePlayer($_POST['tID'], $_POST['pName'], $_POST['pPosition'], $_POST['pReceptions'], $_POST['pReceptionYards'], $_POST['pReceptionTargets'], $_POST['pRushingAttempts'], $_POST['pRushingYards'], $_POST['pRushingYardsPerAttempt'], $_POST['pID'])) {
         echo '<div class="alert alert-success" role="alert">Player Edited!</div>';
       }
       else {
