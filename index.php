f0898f2b4d471a450461f0d02c4f6422a6021ccf
Marcosreza0 <122900443+Marcosreza0@users.noreply.github.com>
2023-12-04 20:33:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,22 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-
-<h1>NFL Player Stats</h1>
-
 <style>
   body {
-    background-image: url('https://oladino.com/wp-content/uploads/2022/02/2021-NFL-Team-Logo-Bundle-Svg-TB200122003.png'); /* Replace with the direct image URL */
+    background-image: url('https://oladino.com/wp-content/uploads/2022/02/2021-NFL-Team-Logo-Bundle-Svg-TB200122003.png');
     background-size: cover;
     background-position: center center;
     /* Add more background properties if needed */
   }
+
+  h1 {
+    padding-top: 50px; /* Adjust the value to move the image down */
+  }
 </style>
 
-
+<h1>NFL Player Stats</h1>
 
 <?php
 include "view-footer.php";
 ?>
-
 
