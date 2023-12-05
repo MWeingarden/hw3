2be23715f72aa2b49bd3fe7cd4a52c3de12fad7a
Marcosreza0 <122900443+Marcosreza0@users.noreply.github.com>
2023-12-04 20:48:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "view-header.php";
 <style>
   body {
     background-image: url('https://oladino.com/wp-content/uploads/2022/02/2021-NFL-Team-Logo-Bundle-Svg-TB200122003.png');
-    background-size: 200px;
+    background-size: 600px;
     background-position: center center;
     /* Add more background properties if needed */
   }
