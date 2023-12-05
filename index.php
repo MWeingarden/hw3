f678d1fa1ea867d731004c56e1c2e05f563d641f
Marcosreza0 <122900443+Marcosreza0@users.noreply.github.com>
2023-12-04 20:26:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include "view-header.php";
 
 <style>
   body {
-    background-image: url('https://www.example.com/path/to/your/image.jpg'); /* Replace with the direct image URL */
+    background-image: url('https://oladino.com/wp-content/uploads/2022/02/2021-NFL-Team-Logo-Bundle-Svg-TB200122003.png'); /* Replace with the direct image URL */
     background-size: cover;
     background-position: center center;
     /* Add more background properties if needed */
