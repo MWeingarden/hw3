091fee960651f6d63d11ecb6e15e30b87071df3b
Marcosreza0 <122900443+Marcosreza0@users.noreply.github.com>
2023-12-04 20:19:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,33 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
+
+<style>
+  body {
+    background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.valenciatheaterseating.com%2Fnational-football-league-teams-NFL-team-icons-Set-all-the-new-8021-104584&psig=AOvVaw1j-jqG8tOJ7Uhclv4OurNy&ust=1701828653909000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDi__Kb94IDFQAAAAAdAAAAABAF'); /* Replace 'your-image-url' with the actual image URL */
+    background-size: cover; /* Adjust as needed */
+    background-position: center center; /* Adjust as needed */
+    /* You can also add more background properties as needed */
+  }
+</style>
+
+<h1>NFL Player Stats</h1>
+
+<?php
+include "view-footer.php";
+?>
+
+
+
+
+
+
+
+
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
 <h1>NFL Player Stats</h1>
 <?php
 include "view-footer.php";
